36de71a65cc073e0bb9a9d902c8ecfd072019447
prajktamhase <mhaseprajkta@gmail.com>
2023-09-30 15:31:25 +0800

    configarable UI

--- src/view/Home/Home.js
+++ src/view/Home/Home.js
@@ -1,13 +1,29 @@
-import Util from "./../view/Util/Util.json";
-function Home(){
-    return(
+import React from "react";
+import "./Home.css";
+
+import Util from "./../../view/Util/Util.json";
+function Home() {
+    return (
         <>
-        <div>
-            <h1>{Util.title}</h1>
-            <h2>{Util.description}</h2>
-            <h3>{Util.price}</h3>
-        </div>
+         <h1 className="center">Sweet Shop</h1>
+ 
+            {
+                Util.map((serviceData, index) => {
+                    const { title, description, price } = serviceData
+                    return (
+                       
+                        <div key={index} className="text">
+                           
+                                <h1>{title}😋</h1>
+                                <h2>{description}😍</h2>
+                                <h3>Price:{price}💸</h3>
+
+
+                        </div>
+                    )
+                })
+            } 
         </>
-        )
+    )
 }
 export default Home
